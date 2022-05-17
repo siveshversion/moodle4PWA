e8f5340892b558ed4bec19c17747416b4c2c6f91
Siveshversion <SIVESHVERSION@GMAIL.COM>
2022-05-17 10:55:49 +0530

    removed spec.ts files

--- src/app/fix-url.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FixUrlPipe } from './fix-url.pipe';
-
-describe('FixUrlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FixUrlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
