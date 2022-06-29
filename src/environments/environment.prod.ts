59bbadc20f4c272dd1b478cdf85e49a5fd4cd161
Siveshversion <SIVESHVERSION@GMAIL.COM>
2022-06-29 12:27:19 +0530

    production url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   production: true,
   MOODLE_TOKEN: '6c55a6c123a01a545efc8c20fd3a0d8a',
   moodle_url: 'http://localhost/moodle4',
+  // moodle_url: 'https://biz.learnospace.com/Clients/moodle400',
   api_path: '/cm/api/methods.php',
 };
